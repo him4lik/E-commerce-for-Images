357543b893b07b9ae3e3b60af140731e01333fc9
Himalik <75934883+him4lik@users.noreply.github.com>
2021-07-29 15:25:00 +0530

    added email functionality

--- hire/views.py
+++ hire/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from custom_auth.models import User
 from django import views
 from .forms import hire_form
+from django.core.mail import send_mail
 # Create your views here.
 
 class hire_inquiry(views.View):
@@ -20,5 +21,6 @@ class hire_inquiry(views.View):
 		artist=User.objects.get(image_to_sell__id=idd)
 		context={'form':fm,'artist': artist}
 		if fm.is_valid():
+			send_mail('Hiring Inquiry by'+ request.user.first_name, fm.cleaned_data['message'], settings.EMAIL_HOST_USER, [artist.contact_email], fail_silentlly=False)
 			context['flag']=1
 		return render(request, self.template_name, context)
